56a6b998ec4c459ef1344716d7a79dbf718bcd88
FRIDAUS <ayobamifridaus@gmail.com>
2024-08-13 02:11:22 +0100

    swagger documentation

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const connectDB = (url) => {
-	return mongoose.connect(url).then(() => console.log('CONNECTED TO DB...')).catch((err) =>console.log(err.message) )
+	return mongoose.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}).then(() => console.log('CONNECTED TO DB...')).catch((err) =>console.log(err.message) )
 }
 
 module.exports = connectDB
