9cd00ba359c4600661d6a59b4133c62721e04d00
FRIDAUS <ayobamifridaus@gmail.com>
2024-08-13 02:33:43 +0100

    jobs api

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const rateLimiter = require('express-rate-limit')
 // Swagger
 const swaggerUI = require('swagger-ui-express')
 const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express')
 const app = express()
 //conectDB
@@ -42,8 +44,9 @@ const errorHandlerMiddleWare = require('./middleware/error-handler')
 
 //routes
 app.get('/', (req, res) => {
-	res.send('<h1> JOB API IS WORKING FINE<h1>')
+	res.send('<h1> JOB API IS WORKING FINE<h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs',authenticationMiddleWare, jobsRouter)
 
